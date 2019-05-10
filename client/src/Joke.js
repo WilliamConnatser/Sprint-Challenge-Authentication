f8e00216d72443bc29df04c54149a3749cfd1229
WilliamConnatser <WilliamConnatser@users.noreply.github.com>
2019-05-10 13:49:18 -0500

    full react functionality complete

--- client/src/Joke.js
+++ client/src/Joke.js
@@ -1,10 +1,26 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
+import axios from 'axios';
 
 export default class Joke extends Component {
+    state={
+        jokes: []
+    }
+
+    componentDidMount() {
+        return axios
+            .get('http://localhost:5000/api/jokes')
+            .then(response => {
+                this.setState({jokes: response.data})
+            })
+            .catch(error => {
+                alert(error.response.data.message)});
+    }
   render() {
+      const jokeElements = this.state.jokes && this.state.jokes.map(joke => <h4 key={joke.id}>{joke.joke}</h4>)
     return (
       <div>
         <h2>Jokes</h2>
+        {jokeElements}
       </div>
     )
   }
